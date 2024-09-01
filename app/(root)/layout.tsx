e82b976828f3508e39026517331d6447d48dc888
Pragyan-Hazarika <136066478+Pragyan-Hazarika@users.noreply.github.com>
2024-09-01 19:03:51 +0530

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,16 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedInc = { firstName: "Jyotirmoy", lastName: "Barman" };
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font inter">
+      <Sidebar user={loggedInc} />
+      {children}
     </main>
   );
 }
