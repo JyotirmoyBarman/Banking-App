d54b6244c30becaa2129814c3d85527ec8f0c121
Pragyan-Hazarika <136066478+Pragyan-Hazarika@users.noreply.github.com>
2024-09-01 19:02:46 +0530

    Update TotalBalanceBox.tsx

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,31 +1,28 @@
-
-import AnimatedCounter from './AnimatedCounter'
+import AnimatedCounter from "./AnimatedCounter";
+import DoughnutChart from "./DoughnutChart";
 
 const TotalBalanceBox = ({
-    accounts= [], totalBanks, totalCurrentBalance
+  accounts = [],
+  totalBanks,
+  totalCurrentBalance,
 }: TotalBalanceBoxProps) => {
-    return(
-        <section className="total-balance">
-            <div className="total-balance-chart">
-                {/* DoughnutChart */}
-            </div>
+  return (
+    <section className="total-balance">
+      <div className="total-balance-chart">
+        <DoughnutChart accounts={accounts} />
+      </div>
 
-            <div className ="flex flex-col gap-6">
-                <h2 className ="header-2">
-                Bank Accounts: {totalBanks} 
-                </h2>
-                <div className="flex flex-col gap-2">
-                    <p className="total-balance-label">
-                        Total Current Balance
-                    </p>
-                    <p className="total-balance-amount flex-center gap-2">
-                        <AnimatedCounter amount=
-                        {totalCurrentBalance}/>
-                    </p>
-                </div>
-            </div>
-        </section>
-    )
-}
+      <div className="flex flex-col gap-6">
+        <h2 className="header-2">Bank Accounts: {totalBanks}</h2>
+        <div className="flex flex-col gap-2">
+          <p className="total-balance-label">Total Current Balance</p>
+          <div className="total-balance-amount flex-center gap-2">
+            <AnimatedCounter amount={totalCurrentBalance} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default TotalBalanceBox
+export default TotalBalanceBox;
