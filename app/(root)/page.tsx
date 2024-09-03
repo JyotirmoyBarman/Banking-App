4680c07728507ede5d56042e4b8676c64b09e6e2
Pragyan-Hazarika <136066478+Pragyan-Hazarika@users.noreply.github.com>
2024-09-03 18:30:49 +0530

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,35 @@
-import React from 'react'
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox';
+import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 const Home = () => {
-    const loggedIn = { firstName : 'Biryani'};
-    return(
-        <section className="home">
-            <div className="home-content">
-                <header className="home-header">
-                    <HeaderBox
-                    type="greeting"
-                    title="Welcome"
-                    user={loggedIn?.firstName || 'Guest'}
-                    subtext="Access and manage your account and transaction efficiently"
-                    />
-                    <TotalBalanceBox
-                      accounts={[]}
-                      totalBanks={1}
-                      totalCurrentBalance={999999.99}
-                    />
-                </header>
+  const loggedIn = {
+    firstName: "Jyotirmoy",
+    lastName: "Barman",
+    email: "jyotirmoybarman@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transaction efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={999999.99}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
 
-            </div>
-        </section>
-    )
-}
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
