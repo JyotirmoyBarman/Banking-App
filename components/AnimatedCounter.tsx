aa0b8a1ecce80cf9296c6249121b667fe0dda0bc
unknown <jyotirmoy157@gmail.com>
2024-09-04 12:50:59 +0530

    new animation time

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -5,7 +5,7 @@ const AnimatedCounter = ({ amount }: { amount: number}) => {
     return (
         <div className="w-full">
             <CountUp 
-            duration={4}
+            duration={0.25}
             decimals={2}
             decimal="."
             prefix="₹"
